1d51b3a5cde82a9371d29e48fb87215e5375ebfe
muak4 <muak@Arbisofts-MacBook-Pro.local>
2025-01-23 02:22:50 +0500

    Add console in database connfig for testing

--- src/database/database-config.service.ts
+++ src/database/database-config.service.ts
@@ -2,28 +2,33 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class DatabaseConfigService {
-  // Example: Retrieve these from environment variables or your configuration file
   get DB_HOST(): string {
+    console.log('DB_HOST:', process.env.DB_HOST); // Log for debugging
     return process.env.DB_HOST || 'localhost';
   }
 
   get DB_PORT(): number {
+    console.log('DB_PORT:', process.env.DB_PORT); // Log for debugging
     return Number(process.env.DB_PORT) || 5432;
   }
 
   get DB_USERNAME(): string {
+    console.log('DB_USERNAME:', process.env.DB_USERNAME); // Log for debugging
     return process.env.DB_USERNAME || 'postgres';
   }
 
   get DB_PASSWORD(): string {
+    console.log('DB_PASSWORD:', process.env.DB_PASSWORD); // Log for debugging
     return process.env.DB_PASSWORD || 'password';
   }
 
   get DB_NAME(): string {
+    console.log('DB_NAME:', process.env.DB_NAME); // Log for debugging
     return process.env.DB_NAME || 'postgres';
   }
 
   get DB_TIMEZONE(): string {
-    return process.env.DB_TIMEZONE || 'UTC'; // Default to UTC
+    console.log('DB_TIMEZONE:', process.env.DB_TIMEZONE); // Log for debugging
+    return process.env.DB_TIMEZONE || 'UTC';
   }
 }
